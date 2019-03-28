74b7ce3f2c1468ca14dbdf41e42ce0040d6da249
Chiheb Bouzid <chihebouzid@gmail.com>
2019-03-28 13:37:52 +0100

    initial setup and style fixes

--- src/App.js
+++ src/App.js
@@ -1,27 +1,85 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import DrumPad from './components/DrumPad'
 import './App.css';
 
+
+
+const soundsDB = [{
+  keyCode: 81,
+  keyTrigger: 'Q',
+  id: 'Heater-1',
+  url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'
+}, {
+  keyCode: 87,
+  keyTrigger: 'W',
+  id: 'Heater-2',
+  url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'
+}, {
+  keyCode: 69,
+  keyTrigger: 'E',
+  id: 'Heater-3',
+  url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'
+}, {
+  keyCode: 65,
+  keyTrigger: 'A',
+  id: 'Heater-4',
+  url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'
+}, {
+  keyCode: 83,
+  keyTrigger: 'S',
+  id: 'Clap',
+  url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'
+}, {
+  keyCode: 68,
+  keyTrigger: 'D',
+  id: 'Open-HH',
+  url: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'
+}, {
+  keyCode: 90,
+  keyTrigger: 'Z',
+  id: "Kick-n'-Hat",
+  url: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'
+}, {
+  keyCode: 88,
+  keyTrigger: 'X',
+  id: 'Kick',
+  url: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'
+}, {
+  keyCode: 67,
+  keyTrigger: 'C',
+  id: 'Closed-HH',
+  url: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'
+},
+];
+
+
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      display: "Play some music !"
+    }
+  }
+  updateDisplay = display => this.setState({ display })
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div id="drum-machine" >
+        <div id="display">{this.state.display}</div>
+        <div id="drum-pads">{soundsDB.map(curr => (
+          <DrumPad
+            keyTrigger={curr.keyTrigger}
+            src={curr.url}
+            soundName={curr.id}
+            keyCode={curr.keyCode}
+            display={this.updateDisplay}
+          />
+
+        ))}
+
+        </div>
       </div>
-    );
+    )
   }
 }
 
---
+++ src/components/DrumPad.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+
+class DrumPad extends Component {
+    constructor(props) {
+        super(props);
+
+        this.playSound = this.playSound.bind(this);
+        this.handleKeyPress = this.handleKeyPress.bind(this);
+
+    }
+
+    componentDidMount() {
+        document.addEventListener('keydown', this.handleKeyPress);
+
+    }
+    componentWillUnmount() {
+        document.removeEventListener('keydown', this.handleKeyPress);
+
+    }
+    handleKeyPress(e) {
+        if (e.keyCode === this.props.keyCode) {
+
+            this.playSound();
+            this.props.display(this.props.soundName);
+        }
+    }
+
+
+
+    playSound() {
+        const sound = document.getElementById(this.props.keyTrigger);
+        sound.play();
+        this.props.display(this.props.soundName);
+    }
+
+
+
+    render() {
+
+        return (
+            <div id="drum-pad-container">
+
+                <div id={this.props.soundName}
+                    className="drum-pad"
+                    onClick={this.playSound}>
+
+                    <audio className='clip'
+                        id={this.props.keyTrigger}
+                        src={this.props.src}
+
+                    ></audio>
+
+                    {this.props.keyTrigger}</div>
+
+            </div>
+        )
+
+    }
+}
+
+export default DrumPad;
